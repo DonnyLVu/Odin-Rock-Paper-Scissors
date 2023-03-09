a65bd6346e19875d3009695ff55ac6e466e6dea5
DonnyLVu <donnyvu897@gmail.com>
2023-03-08 19:21:13 -0800

    add current save for now

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Computer Choice
 
 function computerChoice() {
-  const randomNumber = randomNumber.Math.ceil(3);
-  console.log(randomNumber);
+  const randomNumber = randomNumber.Math.ceil(Math.random() * 3);
+  let computerOutcome;
+  if (randomNumber === 1) computerOutcome = "Rock";
+  if (randomNumber === 2) computerOutcome = "Paper";
+  if (randomNumber === 3) computerOutcome = "Scissor";
+
+  return console.log(randomNumber, computerOutcome);
 }
